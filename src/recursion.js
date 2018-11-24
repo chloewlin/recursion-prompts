e06fb01b6e4054e5e8e9d2103ad772a528152c89
Chloe Lin <chloewjlin@gmail.com>
2018-11-24 05:07:04 -0800

    Complete createArray

--- src/recursion.js
+++ src/recursion.js
@@ -186,7 +186,12 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  if (str.length === 1) {
+    return [str];
+  }
+  return [str[0]].concat(createArray(str.slice(1)))
 };
+// console.log(createArray("str")); // ["s", "t", "r"]
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {

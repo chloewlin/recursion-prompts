b661d730520797348775a8e072773d0a42125d08
Chloe Lin <chloewjlin@gmail.com>
2018-11-26 19:22:07 -0800

    Complete letterTally

--- src/recursion.js
+++ src/recursion.js
@@ -416,8 +416,15 @@ var flatten = function(array) {
 
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
-var letterTally = function(str, obj) {
+var letterTally = function(str, obj = {}) {
+  if (str.length === 0) {
+    return obj;
+  } else {
+    obj[str[0]] = obj[str[0]]++ || 1;
+  }
+  return letterTally(str.slice(1), obj);
 };
+// console.log(letterTally('potato', {}));
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
 // elements they should be replaced with a single copy of the element. The order of the
